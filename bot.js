5615f310359455b4f3b7010d9e3663ea63543755
theooo26 <35627078+theooo26@users.noreply.github.com>
2018-01-22 18:12:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
+    if (message.content === 're') {
+    	message.reply('ReCoucou ! J'espere que tu vas bien ;D');
   	}
 });
 
